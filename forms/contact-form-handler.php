2c389e7e4d4749843b8a2119b508611a4bac9525
Sejal Choudhary <sejalc230@gmail.com>
2021-03-14 15:29:19 +0530

    Removed Contact form

--- forms/contact-form-handler.php
+++ forms/contact-form-handler.php
@@ -1,47 +1,52 @@
 <?php
-$errors = '';
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+//require 'vendor/autoload.php';
+
+//Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 $myemail = 'sejalc230@gmail.com';
-if(empty($_POST['name'])  ||
-   empty($_POST['email']) ||
-   empty($_POST['msg_subject']) ||
-   empty($_POST['message']))
-{
-    $errors .= "\n Error: all fields are required";
-}
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-$subject = $_POST['msg_subject'];
+$subject = $_POST['subject'];
 $message = $_POST['message'];
-if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
-$email_address))
-{
-    $errors .= "\n Error: Invalid email address";
-}
+try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    //$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    //$mail->Username   = 'sejalc230@gmail.com';                     //SMTP username
+    //$mail->Password   = '';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-if(empty($errors))
-{
-$to = $myemail;
-$email_subject = $subject;
-$email_body = "You have received a new message. ".
-" Here are the details:\n Name: $name \n ".
-"Email: $email_address\n Message \n $message";
-$header = "From: noreply@example.com\r\n";
-$header.= "MIME-Version: 1.0\r\n";
-$header.= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-$header.= "X-Priority: 1\r\n";
-$mail_status = mail($to,$email_subject,$email_body,$header);
-if ($mail_status) { ?>
-  <script language="javascript" type="text/javascript">
-   alert('Thank you for the message.');
-  </script>
-  <?php
-  }else { ?>
-   <script language="javascript" type="text/javascript">
-    alert('Message failed. Please, send an email to sejalc230@gmail.com');
-   </script>
-  <?php }
-//header('Location: sejal129.github.io');
-}
+    //Recipients
+    $mail->setFrom($email_address);
+    //$mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+    //$mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo($myemail);
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
 
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $message;
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
